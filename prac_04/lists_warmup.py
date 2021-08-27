199e37dd780fd54d2e23f95439fb52029ad753c1
KaiMeloni <88090392+KaiMeloni@users.noreply.github.com>
2021-08-27 19:17:19 +1000

    Finish prac_04

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,15 +1,21 @@
+# What values do the following expressions have?
+"""3, 1, 4, 1, 5, 9, 2"""
+
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-""" idk """
-
-numbers[0] = "ten"
-numbers[-1] = 1
-numbers[2:]
+numbers[0]
+numbers[-1]
 numbers[3]
 numbers[:-1]
 numbers[3:4]
 5 in numbers
 7 in numbers
-9 in numbers
 "3" in numbers
 numbers + [6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+numbers[2:]
+9 in numbers + [6, 5, 3]
+
+print(numbers)
