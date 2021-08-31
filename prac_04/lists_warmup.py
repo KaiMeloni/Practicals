a833bb633c257d51de10bce9821d511a8569055a
KaiMeloni <88090392+KaiMeloni@users.noreply.github.com>
2021-08-31 12:03:39 +1000

    Add list_warmup comments

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,17 +1,17 @@
 # What values do the following expressions have?
-"""3, 1, 4, 1, 5, 9, 2"""
+# 3, 1, 4, 1, 5, 9, 2
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3:4]
-5 in numbers
-7 in numbers
-"3" in numbers
-numbers + [6, 5, 3]
+numbers[0]  # 3
+numbers[-1]  # null
+numbers[3]  # 1
+numbers[:-1]  # everything except last number
+numbers[3:4]  # start at 4, stop at 1
+5 in numbers  # 9
+7 in numbers  # 0
+"3" in numbers  #
+numbers + [6, 5, 3]  # 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
 
 numbers[0] = "ten"
 numbers[-1] = 1
