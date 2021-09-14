52e70c1be48089e99e5208411bce4a0c398bc0fd
KaiMeloni <88090392+KaiMeloni@users.noreply.github.com>
2021-09-14 10:44:23 +1000

    Added guitar, guitar_test and guitars. Completed class for guitar.

---
+++ prac_06/guitar.py
@@ -0,0 +1,24 @@
+# Design a Guitar class.
+
+CURRENT_YEAR = 2021
+VINTAGE_AGE = 50
+
+
+# Guitar class to store guitar details.
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)  # 2f format specifier.
+
+    def get_age(self):
+        return CURRENT_YEAR - self.year  # Using current year get age of the guitar.
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE  # Determine if the guitar is vintage or not
+
+    def __lt__(self, other):
+        return self.year < other.year  # Sort guitars by the year they are released
