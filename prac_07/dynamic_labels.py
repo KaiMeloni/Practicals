725078c9e1fb22eab05ef2ad1101290511ce2aef
KaiMeloni <88090392+KaiMeloni@users.noreply.github.com>
2021-09-23 11:16:14 +1000

    Added dynamic labels.

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -5,14 +5,10 @@ from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
-    """Main program - Kivy app to demo dynamic widget creation."""
     status_text = StringProperty()
 
     def __init__(self, **kwargs):
-        """Construct main app."""
         super().__init__(**kwargs)
-        # basic data example - dictionary of names: phone numbers
-        # TODO: After running it, add another entry to the dictionary and see how the layout changes
         self.list_of_names = {"Bob Brown", "Cat Cyan", "Oren Ochre", "John Doe"}
 
     def build(self):
