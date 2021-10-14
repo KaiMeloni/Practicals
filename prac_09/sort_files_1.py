96f097ddb5f519d2d44c01479fe3a99572e49250
KaiMeloni <88090392+KaiMeloni@users.noreply.github.com>
2021-10-14 16:47:16 +1000

    Finished sort files.

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,19 @@
+import os
+
+
+def main():
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+
+        file_type = filename.split('.')[-1]
+        try:
+            os.mkdir(file_type)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(file_type, filename))
+        os.rename(filename, "{}/{}".format(file_type, filename))
+
+
+main()
